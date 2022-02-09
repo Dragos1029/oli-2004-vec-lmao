26ddabcc9da43de249289b6c0b3e0959355bd981
Dragos1029 <dragos.morar@avramiancucluj.ro>
2022-02-09 09:50:38 +0200

    almost done

--- oli 2004 vec lmao.cpp
+++ oli 2004 vec lmao.cpp
@@ -6,7 +6,22 @@ using namespace std;
 ifstream in("sir.in");
 ofstream out("sir.out");
 
+int n, nr, cifVec[10];
+
+void citeste() {
+    in >> nr;
+    while (nr > 0)
+    {
+        if (nr % 10 != 0)
+            cifVec[nr % 10]++;
+        nr /= 10;
+    }
+}
+
 int main()
 {
-    out << "Hello World!\n";
+    in >> n;
+    for (int i = 1; i <= n; i++)
+        citeste();
+
 }
