b64e51ed20f4bde339cf9a43df1c0a9b15749ece
Dragos1029 <dragos.morar@avramiancucluj.ro>
2022-02-09 10:24:17 +0200

    done

--- oli 2004 vec lmao.cpp
+++ oli 2004 vec lmao.cpp
@@ -6,22 +6,49 @@ using namespace std;
 ifstream in("sir.in");
 ofstream out("sir.out");
 
-int n, nr, cifVec[10];
+int n, cifVec[10], nrGen, aux, nr;
+
+void citesteVec(int mNr) {
+    while (mNr > 0)
+    {
+        if (mNr % 10 != 0)
+            cifVec[mNr % 10]++;
+        mNr /= 10;
+    }
+}
+
+void genereazaNr() {
+    nrGen = 0;
+    for (int i = 1; i <= 9; i++) {
+        if (cifVec[i] != 0) {
+            nrGen *= 10;
+            nrGen += cifVec[i];
+        }
+    }
+}
+
+void resetVec() {
+    for (int i = 1; i <= 9; i++)
+        cifVec[i] = 0;
+}
 
 void citeste() {
-    in >> nr;
-    while (nr > 0)
-    {
-        if (nr % 10 != 0)
-            cifVec[nr % 10]++;
-        nr /= 10;
+    in >> n;
+    for (int i = 1; i <= n; i++) {
+        in >> nr;
+        citesteVec(nr);
     }
 }
 
 int main()
 {
-    in >> n;
-    for (int i = 1; i <= n; i++)
-        citeste();
-
+    citeste();
+    genereazaNr();
+    while (nrGen > 9)
+    {
+        resetVec();
+        citesteVec(nrGen);
+        genereazaNr();
+    }
+    out << nrGen;
 }
